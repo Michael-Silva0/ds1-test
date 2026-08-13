8922d5e5e6af720f13e5902129f7c8273a926fd0
Michael-Silva0 <caiomichael4559@gmail.com>
2026-08-12 22:32:16 -0300

    Aula 12/8

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__) 
 
 
+    
 @app.route('/')
 def home():
-    return "<h1>Servidos Flask rodando!</h1>" "<h1>Bem vindo ao meu servidor flask</h1>"
-    #return render_template("index.html")
+    #return "<h1>Servidos Flask rodando!</h1>" "<h1>Bem vindo ao meu servidor flask</h1>"
+    return render_template("index.html")
     
 
 @app.route('/sobre')
@@ -18,6 +19,22 @@ def sobre():
 @app.route('/status')
 def status():
     return "<h1>Estatus da aplicação</h1>" "<p>O servidor esta rodando flask corretamente</p>"
+
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+
+@app.route("/salvar", methods=["POST"])
+def salvar():
+    nome_digitado = request.form.get("campo_nome")
+    info_digitada = request.form.get("campo_info")
+
+    return render_template("resultado.html", nome=nome_digitado, info=info_digitada)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
